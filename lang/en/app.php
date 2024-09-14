2e95a6f455e412b53f6840ce698af78fc040fcb0
kristian <krestrepoo@eafit.edu.co>
2024-09-14 13:15:55 -0500

    feat: add lang functionality to the layout template

---
+++ lang/en/app.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'home' => 'Home',
+    'about' => 'About',
+    'products' => 'Products',
+    'search' => 'Search...',
+    'get_started' => 'Get started',
+    'copyright' => '©2024 Evelyn Zapata, Ana Sofía Rodríguez, Kristian Restrepo. All rights reserved.',
+    'title' => 'Online store',
+    'navbar_title' => 'Tonalí',
+    'icon_alt' => 'Icon of Tonalí',
+    'combination_mark_alt' => 'Combination mark logo of Tonalí',
+];

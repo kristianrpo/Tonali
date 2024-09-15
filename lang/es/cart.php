c1b52d67a9ed760c88f4b191a177903d030fffb0
kristian <krestrepoo@eafit.edu.co>
2024-09-15 17:07:41 -0500

    feat: add shopping cart view with ajax and lang

---
+++ lang/es/cart.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => 'Carrito de Compras',
+    'remove' => 'Eliminar',
+    'order_summary' => 'Resumen del Pedido',
+    'total' => 'Total',
+    'continue_shopping' => 'Continuar Comprando',
+    'products' => 'Productos',
+    'quantity' => 'Cantidad',
+    'price' => 'Precio',
+    'purchase' => 'Comprar',
+    'or' => 'o',
+];
---
+++ public/js/cart/update.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('.decrease-quantity, .increase-quantity').click(function (e) {
+        e.preventDefault();
+        let productId = $(this).data('id');
+        let quantitySpan = $('span[data-id="' + productId + '"]');
+        let quantity = parseInt(quantitySpan.text());
+
+        if ($(this).hasClass('increase-quantity')) {
+            quantity++;
+        } else {
+            if (quantity > 1) {
+                quantity--;
+            }
+        }
+        $.ajax({
+            type: 'POST',
+            url: cartUpdateUrl,
+            data: {
+                _token: csrfToken,
+                id: productId,
+                quantity: quantity,
+            },
+            success: function (response) {
+                console.log(response);
+                quantitySpan.text(quantity);
+                $('.summary-quantity-' + productId).text(quantity);
+                $('.total-price').text(response.total);
+            }
+        });
+    });
+});

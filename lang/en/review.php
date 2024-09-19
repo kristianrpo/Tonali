da167301ec7049d1bf7d9ec9a8f5bba56a9b5bf0
kristian <krestrepoo@eafit.edu.co>
2024-09-18 20:47:51 -0500

    feat: add lang to review views

---
+++ lang/en/review.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'reviews' => 'Reviews',
+    'total_score' => 'Total Score',
+    'create_review' => 'Create review',
+    'edit_review' => 'Edit Review',
+    'rating' => 'Rating',
+    'title_form' => 'Review Title',
+    'description_form' => 'Review Description',
+    'create_button' => 'Create Review',
+    'edit_button' => 'Edit Review',
+    'cancel_button' => 'Cancel',
+    'delete_confirmation' => 'Are you sure you want to delete this review?',
+];
---
+++ lang/es/review.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'reviews' => 'Reseñas',
+    'total_score' => 'Puntuación Total',
+    'create_review' => 'Crear Reseña',
+    'edit_review' => 'Editar Reseña',
+    'rating' => 'Puntuación',
+    'title_form' => 'Título de la Reseña',
+    'description_form' => 'Descripción de la Reseña',
+    'create_button' => 'Crear Reseña',
+    'edit_button' => 'Editar Reseña',
+    'cancel_button' => 'Cancelar',
+    'delete_confirmation' => '¿Estás seguro de que quieres eliminar esta reseña?',
+];

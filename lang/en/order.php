4add84869248e8bf4258d6aba1e6228c325d93c5
kristian <krestrepoo@eafit.edu.co>
2024-09-20 18:35:06 -0500

    feat: add lang to order index page

---
+++ lang/en/order.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'order' => 'Order',
+    'product_name' => 'Product Name',
+    'quantity' => 'Quantity',
+    'price' => 'Price',
+    'id' => 'ID',
+    'notification' => 'Notification',
+    'description_notification' => 'You have no orders placed, buy products to see your orders here :)',
+];
---
+++ lang/es/order.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'order' => 'Orden',
+    'product_name' => 'Nombre del Producto',
+    'quantity' => 'Cantidad',
+    'price' => 'Precio',
+    'id' => 'ID',
+    'notification' => 'Notificación',
+    'description_notification' => 'No tienes órdenes realizadas, compra productos para ver tus órdenes aquí :)',
+];

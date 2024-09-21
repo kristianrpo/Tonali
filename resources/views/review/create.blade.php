23eb20923ec4a5850bf943bc029f3e6ed2a684f6
kristian <krestrepoo@eafit.edu.co>
2024-09-20 22:30:39 -0500

    feat: add error notification in the view when creating or modifying a review

--- resources/views/review/create.blade.php
+++ resources/views/review/create.blade.php
@@ -1,5 +1,42 @@
 @extends("layouts.app")
+@section("title", __("review.create_review"))
 @section("content")
+    @if ($errors->any())
+        <div class="mb-10 flex justify-center">
+            <div
+                class="w-3/4 rounded-b bg-palePink px-4 py-3 text-offWhite shadow-md"
+                role="alert"
+            >
+                <div class="flex">
+                    <div class="mx-5 py-1">
+                        <svg
+                            class="mr-4 h-6 w-6 fill-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"
+                            />
+                        </svg>
+                    </div>
+                    <div>
+                        <p class="font-bold">
+                            {{ __("review.notification") }}
+                        </p>
+                        <ul
+                            id="errors"
+                            class="alert alert-danger list-unstyled"
+                        >
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+
     <div class="flex justify-center">
         <div class="relative max-h-full w-full max-w-2xl p-4">
             <div class="relative rounded-lg bg-white shadow">

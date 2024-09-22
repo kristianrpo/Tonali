6b200942d452f949e38d8a11d3fb4a77d3d46fb8
Evelyn <eazapatat@eafit.edu.co>
2024-09-22 11:25:32 -0500

    refactor: correct code styles with pint and format

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
-use Illuminate\Http\JsonResponse;
 
 class ProductController extends Controller
 {
@@ -52,8 +52,9 @@ class ProductController extends Controller
 
         if ($request->ajax()) {
             $suggestions = Product::getSuggestionsByName($query);
+
             return response()->json($suggestions);
-        }        
+        }
 
         $viewData = [];
         $products = Product::searchProducts($query)->get();
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\Builder;
 
 class Product extends Model
 {
@@ -314,6 +314,6 @@ class Product extends Model
 
     public static function getSuggestionsByName(string $query)
     {
-        return Product::where('name', 'like', $query . '%')->distinct()->pluck('name');
+        return Product::where('name', 'like', $query.'%')->distinct()->pluck('name');
     }
 }

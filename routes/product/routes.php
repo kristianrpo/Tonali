6744d414a9d7216624c91e753956fb89b346dfef
kristian <krestrepoo@eafit.edu.co>
2024-09-22 23:47:41 -0500

    refactor: remove duplicate route

--- routes/product/routes.php
+++ routes/product/routes.php
@@ -5,4 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
 Route::get('/products/suggest', 'App\Http\Controllers\ProductController@suggest')->name('product.suggest');
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
-Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');

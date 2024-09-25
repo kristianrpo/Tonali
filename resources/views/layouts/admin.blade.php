aecc8d83c6d3be6195d7dd0e35b6599551b5b4cc
kristian <krestrepoo@eafit.edu.co>
2024-09-24 23:14:42 -0500

    refactor: delete unused buttons

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -129,54 +129,6 @@
         <ul class="space-y-2">
           <li>
             <a
-              href="{{ route("product.index") }}"
-              class="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100"
-            >
-              <svg
-                class="h-6 w-6 text-gray-400 transition duration-75 group-hover:text-gray-900"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M9 10V6a3 3 0 0 1 3-3v0a3 3 0 0 1 3 3v4m3-2 .917 11.923A1 1 0 0 1 17.92 21H6.08a1 1 0 0 1-.997-1.077L6 8h12Z"
-                />
-              </svg>
-
-              <span class="ml-3">
-                {{ __("product.buyer") }}
-              </span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100"
-            >
-              <svg
-                aria-hidden="true"
-                class="h-6 w-6 text-gray-400 transition duration-75 group-hover:text-gray-900"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-              </svg>
-              <span class="ml-3">
-                {{ __("product.overview") }}
-              </span>
-            </a>
-          </li>
-          <li>
-            <a
               href="{{ route("admin.product.index") }}"
               class="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100"
             >

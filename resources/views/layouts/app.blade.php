db2972f311ff689226d0b9c62cea9b318f6b5916
kristian <krestrepoo@eafit.edu.co>
2024-11-15 13:12:56 -0500

    fix: correct navbar buttons to be responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <title>@yield("title", __("layoutApp.title"))</title>
 
     @vite("resources/css/app.css")
+    @vite("resources/js/app.js")
   </head>
 
   <body class="flex min-h-screen flex-col">
@@ -81,6 +82,14 @@
                 {{ __("layoutApp.products") }}
               </a>
             </li>
+            <li>
+              <a
+              href="{{ route("product.recommended") }}"
+              class="block rounded px-3 py-2 text-gray-900 hover:bg-palePink md:p-0 md:hover:bg-transparent md:hover:text-brightPink"
+              >
+                {{ __("layoutApp.recommended") }}
+            </a>
+            </li>
           </ul>
           @guest
             <div class="mx-4 my-2 flex justify-center">
@@ -93,18 +102,6 @@
               </a>
             </div>
           @else
-            <ul
-              class="ml-4 mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse"
-            >
-              <li>
-                <a
-                  href="{{ route("product.recommended") }}"
-                  class="block rounded px-3 py-2 text-gray-900 hover:bg-palePink md:p-0 md:hover:bg-transparent md:hover:text-brightPink"
-                >
-                  {{ __("layoutApp.recommended") }}
-                </a>
-              </li>
-            </ul>
             <div class="mx-10 my-2 flex justify-center">
               <div class="flex items-center space-x-4">
                 <div class="relative">
@@ -163,15 +160,14 @@
                     </form>
                   </div>
                 </div>
+                @endguest
               </div>
-            </div>
-          @endguest
-          <a
+              <a
           href="{{ route("cart.index") }}"
           class="text-darkGray hover:text-black"
           >
             <div
-              class="flex h-10 w-10 items-center justify-center rounded-full border-2 border-darkGray hover:bg-palePink"
+              class="flex h-10 w-10 items-center justify-center rounded-full border-2 border-darkGray hover:bg-palePink mx-2"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -219,6 +215,7 @@
               </svg>
             </div>
           </a>
+            </div>
         </div>
       </div>
     </nav>
@@ -240,6 +237,5 @@
         </span>
       </div>
     </footer>
-    @vite("resources/js/app.js")
   </body>
 </html>

941ed8681c278d6baca6c476cfdc61f745838fce
Evelyn <eazapatat@eafit.edu.co>
2024-11-17 22:43:28 -0500

    feat: add unit test

---
+++ tests/Unit/Product/GetAverageRating.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit\Product;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Product;
+
+class GetAverageRatingTest extends TestCase
+{
+    public function test_get_average_rating_without_reviews(): void
+    {
+        $productMock = $this->getMockBuilder(Product::class)
+            ->onlyMethods(['save'])
+            ->getMock();
+
+        $productMock->method('save')->willReturn(true);
+
+        $productMock->setQuantityReviews(0);
+        $productMock->setSumRatings(0);
+
+        $this->assertEquals(0, $productMock->getAverageRating());
+    }
+}
+

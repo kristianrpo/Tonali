dcdbeb38fb38ea6dbaf79158e79cecbadef4e3ae
Evelyn <eazapatat@eafit.edu.co>
2024-11-19 02:07:25 -0500

    refactor: add icon to edit buttons

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -56,6 +56,23 @@
               type="submit"
               class="bg-primary-700 focus:ring-primary-300 me-2 inline-flex items-center rounded-lg bg-brightPink px-5 py-2.5 text-center text-sm font-medium text-offWhite focus:outline-none focus:ring-4"
             >
+              <svg
+                class="h-6 w-6 text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"
+                />
+              </svg>
               {{ __("user.edit_button") }}
             </button>
           </div>

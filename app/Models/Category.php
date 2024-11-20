c4a3002b207783351c6c3200bf20b5b0beede4a8
kristianrpo <kristianrpo@users.noreply.github.com>
2024-11-20 02:57:54 +0000

    refactor: correct code style issues

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
